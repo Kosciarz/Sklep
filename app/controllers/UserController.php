0832d136ca6cccd68373b359eb73eea4c826a4bf
Bartosz <bartoszbachul1@gmail.com>
2025-03-19 17:45:05 +0100

    changes

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -16,7 +16,7 @@ class UserController extends Controller
     {
         header('Location: rejestracja.php');
         exit();
-        }
+    }
 
     public function log_in(): void
     {
--- app/views/logowanie.php
+++ app/views/logowanie.php
@@ -1,5 +1,7 @@
 <?php
 
+session_start();
+
 require_once __DIR__ . '/../core/Database.php';
 require_once __DIR__ . '/../controllers/UserController.php';
 
